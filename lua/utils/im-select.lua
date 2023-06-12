4ac203bf51998ca87d5ddabbecaa203a6c7602fa
daixiongsheng <daixiongsheng@gmail.com>
2023-06-13 00:00:14 +0800

    2023-06-13-00:00:14 /Users/bytedance/.config/nvim

--- lua/utils/im-select.lua
+++ lua/utils/im-select.lua
@@ -4,16 +4,17 @@ M.defaultIM = "com.apple.keylayout.ABC"
 M.currentIM = M.defaultIM
 
 M.macInsertLeave = function()
-  M.currentIM = vim.fn.system({ "im-select" })
+  -- M.currentIM = vim.fn.system({ "im-select" })
   vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
 end
 
 M.macInsertEnter = function()
-  if M.currentIM then
-    vim.cmd(":silent :!im-select" .. " " .. M.currentIM)
-  else
-    vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
-  end
+  -- if M.currentIM then
+  --   vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
+  -- else
+  --   vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
+  -- end
+  vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
 end
 
 M.windowsInsertLeave = function()
