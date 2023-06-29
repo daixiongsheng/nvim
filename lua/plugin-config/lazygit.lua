19731a38a4879d774210b77e18fd486c0da5c6af
daixiongsheng <daixiongsheng@gmail.com>
2023-06-30 00:00:10 +0800

    2023-06-30-00:00:10 /Users/bytedance/.config/nvim

--- lua/plugin-config/lazygit.lua
+++ lua/plugin-config/lazygit.lua
@@ -1,11 +1,11 @@
-local status, lazygit = pcall(require, "lazygit")
-if not status then
-  vim.notify("没有找到 lazygit")
-  return
-end
-lazygit.setup({
-  "kdheepak/lazygit.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-})
+-- local status, lazygit = pcall(require, "lazygit")
+-- if not status then
+--   vim.notify("没有找到 lazygit")
+--   return
+-- end
+-- lazygit.setup({
+--   "kdheepak/lazygit.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
+-- })
